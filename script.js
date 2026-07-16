1835956d483c925768f82945a92bec038091ff13
enescuvertical <enescumihai723@gmail.com>
2026-07-16 11:59:40 +0200

    Add event listeners for DOMContentLoaded and h1 click

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("EnescuVertical loaded successfully!");
+
+    document.querySelector("h1").addEventListener("click", function () {
+        alert("Welcome to EnescuVertical!");
+    });
+});
